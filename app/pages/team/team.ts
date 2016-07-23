1fa8522e6a09954f8cba84b681da464a45eb6e07
fatrat1117 <fatrat1117@gmail.com>
2016-07-23 17:19:58 +0800

    my teams

---
+++ app/pages/team/team.ts
@@ -0,0 +1,11 @@
+import {Component} from '@angular/core';
+import {NavController} from 'ionic-angular';
+
+@Component({
+  templateUrl: 'build/pages/team/team.html'
+})
+export class TeamPage {
+  constructor(private navCtrl: NavController) {
+  
+  }
+}
