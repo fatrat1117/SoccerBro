45e389c5fb379cdcf81010a0905e266b9587e958
ray-z <710956@gmail.com>
2016-07-24 16:59:59 +0800

    Added dummy data for chat room.

--- app/pages/chat-room/chat-room.ts
+++ app/pages/chat-room/chat-room.ts
@@ -2,7 +2,7 @@ import {Component} from '@angular/core';
 import {NavController} from 'ionic-angular';
 
 @Component({
-  templateUrl: 'build/pages/chat-room/chat-room.html'
+  templateUrl: 'build/pages/chat-room/chat-room.html',
 })
 export class ChatRoomPage {
   constructor(private navCtrl: NavController) {
