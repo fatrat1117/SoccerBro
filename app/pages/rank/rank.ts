de95af9555591241ea86af9875df1675e27dfbe7
fatrat1117 <fatrat1117@gmail.com>
2016-07-25 11:48:43 +0800

    in app brower

--- app/pages/rank/rank.ts
+++ app/pages/rank/rank.ts
@@ -1,10 +1,16 @@
 import {Component} from '@angular/core';
 import {NavController} from 'ionic-angular';
+import {AccountManager} from '../../providers/account-manager'
 
 @Component({
   templateUrl: 'build/pages/rank/rank.html'
 })
 export class RankPage {
-  constructor(private navCtrl: NavController) {
+  constructor(private navCtrl: NavController, private am : AccountManager) {
+
+  }
+
+  OnLogin() {
+      this.am.facebook();
   }
 }
--- app/providers/account-manager.ts
+++ app/providers/account-manager.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import {CordovaOauth, Facebook} from 'ng2-cordova-oauth/core';
-import {AngularFire} from 'angularfire2';
+import {AngularFire, AuthProviders, AuthMethods } from 'angularfire2';
 
 @Injectable()
 export class AccountManager {
@@ -14,21 +14,28 @@ export class AccountManager {
          this.cordovaOauth.login().then(success => {
             console.log("Facebook success: " + JSON.stringify(success));
             console.log(success["access_token"]);
-            var provider = new firebase.auth.FacebookAuthProvider();
-            alert('1');
-            firebase.auth().signInWithCredential(provider.credential(success["access_token"]))
-              .then((success) => {
-                alert('2');
-                console.log("Firebase success: " + JSON.stringify(success));
-              },
-              (error) => {
-                alert('3');
-                console.log("Firebase failure: " + JSON.stringify(error));
-              });
-              // .catch((error) => {
-              //   console.log("Firebase failure: " + JSON.stringify(error));
-              // });
-              alert('4');
+            this.af.auth.login(success["access_token"]).then((value) => {
+            console.log(value);
+            //this.dismiss()
+        }).catch((error) => {
+            //this.error = error
+            console.log(error)
+        });
+            //var provider = new firebase.auth.FacebookAuthProvider();
+            //alert('1');
+            // firebase.auth().signInWithCredential(provider.credential(success["access_token"]))
+            //   .then((success) => {
+            //     alert('2');
+            //     console.log("Firebase success: " + JSON.stringify(success));
+            //   },
+            //   (error) => {
+            //     alert('3');
+            //     console.log("Firebase failure: " + JSON.stringify(error));
+            //   });
+            //   // .catch((error) => {
+            //   //   console.log("Firebase failure: " + JSON.stringify(error));
+            //   // });
+            //   alert('4');
       });
     }
 }
