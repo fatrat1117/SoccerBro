8518855e2a71cc123dc12796d607b7b15d35c45d
ray-z <710956@gmail.com>
2016-07-27 21:34:29 +0800

    Got chat history from Firebase.

--- app/pages/chat-room/chat-room.ts
+++ app/pages/chat-room/chat-room.ts
@@ -1,11 +1,13 @@
 import {Component} from '@angular/core';
 import {NavController} from 'ionic-angular';
+import {AngularFire, FirebaseListObservable} from 'angularfire2';
 
 @Component({
   templateUrl: 'build/pages/chat-room/chat-room.html',
 })
 export class ChatRoomPage {
-  constructor(private navCtrl: NavController) {
-  
+  items: FirebaseListObservable<any[]>;
+  constructor(private navCtrl: NavController, af: AngularFire) {
+    this.items = af.database.list('/chatrooms/-KL1QXqFWsC1Jbb-HXsJ');
   }
 }
