0e79de82da98cb3ce5f0c1b235dd5defabfb3c68
ray-z <710956@gmail.com>
2016-07-30 20:39:12 +0800

    Showed time label according to days ago.

--- app/pages/chat-room/chat-room.ts
+++ app/pages/chat-room/chat-room.ts
@@ -12,29 +12,41 @@ import * as moment from 'moment'
 export class ChatRoomPage {
   items: FirebaseListObservable<any[]>;
   tempTime: number;
-  timeFormat: string;
   today: number;
+  daysAgo: number;
 
   constructor(private navCtrl: NavController, af: AngularFire) {
     this.items = af.database.list('/chatrooms/-KL1QXqFWsC1Jbb-HXsJ');
     this.tempTime = 0;
-    this.timeFormat = "M/DD/YYYY HH:MM";
     this.today = moment().unix() * 1000;
-    //console.log(this.today)
+    this.daysAgo = 0;
   }
 
   showTime(_item) {
-    var isShow = false;
     var current = _item.created_at;
-    var daysAgo = 0;
-    //console.log(moment.duration(this.today-current).asWeeks());
-    console.log(current - this.tempTime);
-    if (current - this.tempTime > 300000)
-    {
-      isShow = true;
-    }
-
+    var isShow = (current - this.tempTime > 300000); // 5 mins
     this.tempTime = current;
     return isShow;
+  }
+
+  getTime(_item) {
+    var newTime: string;
+    var current = _item.created_at;
+    var count = Math.ceil(moment.duration(this.today - current).asDays());
+    if (count != this.daysAgo) {
+      
+      newTime = moment(current).calendar(null, {
+        sameDay: 'HH:MM',
+        lastDay: '[Yesterday] HH:MM',
+        lastWeek: 'ddd HH:MM',
+        sameElse: 'M/DD/YY HH:MM'
+      });
+      this.daysAgo = count;
+    }
+    else {
+      newTime = moment(current).format('HH:MM');
+    }
+    
+    return newTime;
   }
 }
