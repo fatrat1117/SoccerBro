49ee2cf5873f8b6bf239e6d3c222d9b61b76e659
fatrat1117 <fatrat1117@gmail.com>
2016-08-02 16:37:51 +0800

    create team UI

---
+++ app/pages/create-team/create-team.ts
@@ -0,0 +1,11 @@
+import {Component} from '@angular/core';
+import {NavController} from 'ionic-angular';
+
+@Component({
+  templateUrl: 'build/pages/my-team/my-team.html'
+})
+export class MyTeamPage {
+  constructor(private navCtrl: NavController) {
+  
+  }
+}
