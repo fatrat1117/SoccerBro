086bcbb52ba17d8c00eb605a841cc03e6f9c97bd
ray-z <710956@gmail.com>
2016-08-07 11:29:31 +0800

    Fetching notification details.

--- app/pages/message/message.ts
+++ app/pages/message/message.ts
@@ -3,26 +3,38 @@ import {NavController} from 'ionic-angular';
 import {ChatRoomPage} from '../chat-room/chat-room';
 import {AngularFire, FirebaseObjectObservable, FirebaseListObservable} from 'angularfire2';
 
+import { Observable } from 'rxjs/Observable';
+
 declare let firebase: any;
 @Component({
   templateUrl: 'build/pages/message/message.html'
 })
 export class MessagePage {
   message: string;
-  notifications: FirebaseObjectObservable<any>[];
+  notifications = [];
   // firebase
   items: FirebaseListObservable<any[]>;
   
   constructor(private navCtrl: NavController, private af: AngularFire) {
     this.message = "chats";
-    this.notifications = [];
+
     // firebase
+
     this.items = af.database.list('/notifications/VP0ilOBwY1YM9QTzyYeq23B82pR2');
     this.items.subscribe(snapshots => {
       snapshots.forEach(snapshot => {
-        this.notifications.push(this.af.database.object('/notification-contents/'))
+        var n = af.database.object(`/notification-contents/${snapshot.id}`).subscribe(
+          s => {
+            this.notifications.push(s)
+        })
+        //this.notifications.push(af.database.object(`/notification-contents/${snapshot.id}`));
+        //console.log(this.notifications[0]);      
       });
     })
+  }
+
+  getNotification() {
+
   }
 
   enterChatRoom(){
@@ -33,13 +45,20 @@ export class MessagePage {
       return _item.key
   }
 
-  getContent(_id) {
-    console.log(_id);
-    
+  getTitle(_id) {
     return this.af.database.object('/notification-contents/' + _id)
   }
 
   test() {
+    this.af.database.list('/notification-contents/').push({
+      timestamp: firebase.database.ServerValue.TIMESTAMP,
+      title: "new notification",
+      content: 'this is a test notification',
+      creator_id: 'VP0ilOBwY1YM9QTzyYeq23B82pR2',
+      team_id: '-KL1QXqFWsC1Jbb-HXsJ'
+    });
+
+    /*
     var uid = this.uuid();
     this.af.database.object('/notification-contents/' + uid).set({
       timestamp: firebase.database.ServerValue.TIMESTAMP,
@@ -52,6 +71,7 @@ export class MessagePage {
       id: uid,
       isRead: false,
      });
+     */
   }
 
   uuid() {
