fd026fe86786d8fb11777209cdc86039c5b39788
fatrat1117 <fatrat1117@gmail.com>
2016-08-12 14:56:01 +0800

    load cached player and team

--- app/pages/me/me.ts
+++ app/pages/me/me.ts
@@ -1,7 +1,6 @@
 import {Component} from '@angular/core';
 import {NavController, NavParams, Modal} from 'ionic-angular';
 import {AccountManager} from '../../providers/account-manager'
-import {AngularFire, FirebaseObjectObservable} from 'angularfire2';
 import {MyTeamPage} from '../my-team/my-team';
 import {CreateTeamPage} from '../create-team/create-team';
 import {ManageTeamPage} from '../manage-team/manage-team';
@@ -10,20 +9,24 @@ import {ManageTeamPage} from '../manage-team/manage-team';
   templateUrl: 'build/pages/me/me.html'
 })
 export class MePage {
-  player: FirebaseObjectObservable<any>;
-  defaultTeam: FirebaseObjectObservable<any>;
+  //player: FirebaseObjectObservable<any>;
+  //defaultTeam: FirebaseObjectObservable<any>;
+  player: any;
+  defaultTeam: any;
 
-  constructor(private nav: NavController, private am: AccountManager, private af: AngularFire) {
-    let self = this;
-    this.player = this.af.database.object(this.am.getCurrentPlayerRef());
-    //let playerSnapshot = this.af.database.object(this.am.getCurrentPlayerRef(), { preserveSnapshot: true });
-    this.player.subscribe(pSnapshot => {
-      //console.log(self.player);
-      console.log(pSnapshot.currentTeamId);
-      // let val = snapshot.val();
-      // console.log(val);
-      self.defaultTeam = self.af.database.object(self.am.getTeamRef(pSnapshot.currentTeamId));
-    });
+  constructor(private nav: NavController, private am: AccountManager) {
+    this.player = this.am.getCurrentPlayerSnapshot();
+    this.defaultTeam = this.am.getCurrentTeamSnapshot();
+    // let self = this;
+    // this.player = this.af.database.object(this.am.getCurrentPlayerRef());
+    // //let playerSnapshot = this.af.database.object(this.am.getCurrentPlayerRef(), { preserveSnapshot: true });
+    // this.player.subscribe(pSnapshot => {
+    //   //console.log(self.player);
+    //   console.log(pSnapshot.currentTeamId);
+    //   // let val = snapshot.val();
+    //   // console.log(val);
+    //   self.defaultTeam = self.af.database.object(self.am.getTeamRef(pSnapshot.currentTeamId));
+    // });
   }
 
   openNavTeamPage() {
