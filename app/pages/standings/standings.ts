bfed0cb9972e476594fe7794cb4f05c3e3879a29
Lu Angxiao <luangxiao914@gmail.com>
2016-08-12 18:07:06 +0800

    增加积分榜

---
+++ app/pages/standings/standings.ts
@@ -0,0 +1,59 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+/*
+  Generated class for the StandingsPage page.
+
+  See http://ionicframework.com/docs/v2/components/#navigation for more info on
+  Ionic pages and navigation.
+*/
+@Component({
+  templateUrl: 'build/pages/standings/standings.html',
+})
+export class StandingsPage {
+  standings: any[];
+  constructor(private nav: NavController) {
+    this.standings = [
+      {
+        Rank:"1",
+        Logo:"<ion-icon name='ios-football'></ion-icon>",
+        Team: "Chelsea",
+        GP:"38",
+        W:"26",
+        D:"9",
+        L:"3",
+        GF:"73",
+        GA:"32",
+        GD:"41",
+        PTS:"87",
+      },
+      {
+        Rank:"2",
+        Logo:"<ion-icon name='ios-football'></ion-icon>",
+        Team: "Man. City",
+        GP:"38",
+        W:"26",
+        D:"9",
+        L:"3",
+        GF:"73",
+        GA:"32",
+        GD:"41",
+        PTS:"87",
+      },
+      {
+        Rank:"3",
+        Logo:"<ion-icon name='ios-football'></ion-icon>",
+        Team: "Arsenal",
+        GP:"38",
+        W:"26",
+        D:"9",
+        L:"3",
+        GF:"73",
+        GA:"32",
+        GD:"41",
+        PTS:"87",
+      },
+    ];
+  }
+
+}
--- app/pages/stats/stats.ts
+++ app/pages/stats/stats.ts
@@ -1,11 +1,16 @@
 import {Component} from '@angular/core';
 import {NavController} from 'ionic-angular';
 
+import{StandingsPage}from '../standings/standings';
+
 @Component({
   templateUrl: 'build/pages/stats/stats.html'
 })
 export class StatsPage {
   stats: string = "team";
   constructor(private navCtrl: NavController) {
+  }
+  enterStandings(){
+    this.navCtrl.push(StandingsPage);
   }
 }
