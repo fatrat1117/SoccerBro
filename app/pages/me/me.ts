65e77bfd476a3ff73fb0e424671223c97c37c15e
fatrat1117 <fatrat1117@gmail.com>
2016-08-14 16:27:53 +0800

    bind current team
    angular2-google-maps

--- app/pages/me/me.ts
+++ app/pages/me/me.ts
@@ -1,5 +1,6 @@
 import {Component} from '@angular/core';
 import {NavController, NavParams, Modal} from 'ionic-angular';
+import { FirebaseObjectObservable} from 'angularfire2';
 import {AccountManager} from '../../providers/account-manager'
 import {MyTeamPage} from '../my-team/my-team';
 import {CreateTeamPage} from '../create-team/create-team';
@@ -10,16 +11,17 @@ import {ManageTeamPage} from '../manage-team/manage-team';
 })
 export class MePage {
   //player: FirebaseObjectObservable<any>;
-  //defaultTeam: FirebaseObjectObservable<any>;
+  defaultTeam: FirebaseObjectObservable<any>;
   player: any;
-  defaultTeam: any;
+  //defaultTeam: any;
 
   constructor(private nav: NavController, private am: AccountManager) {
     let self = this;
     this.am.afGetCurrentPlayer().subscribe(_ => {
       console.log("current player data changed, update me UI");
       self.player = this.am.getCurrentPlayerSnapshot();
-      self.defaultTeam = this.am.getCurrentTeamSnapshot();
+      if (self.player.currentTeamId)
+        self.defaultTeam = this.am.afGetTeam(self.player.currentTeamId);
     });
   }
 
--- app/providers/account-manager.ts
+++ app/providers/account-manager.ts
@@ -90,24 +90,24 @@ export class AccountManager {
 
 
     //teams of current player 
-    this.afTeamsOfCurrPlayer = this.afGetTeamsOfPlayer(user.uid);
-    let sub3 = this.afTeamsOfCurrPlayer.subscribe(teamIds => {
-      console.log("team of current player change ids", teamIds);
-      self.teamsOfCurrPlayer = [];
-      self.teamsOfCurrPlayer.length = teamIds.length;
-      for (let i = 0; i < teamIds.length; ++i) {
-        let tId = teamIds[i].$key;
-        let afTeam = self.afGetTeam(tId);
-        //console.log(tId, afTeam);
-        let sub4 = afTeam.subscribe(teamSnapshot => {
-          console.log("team snapshot changed", teamSnapshot);
-          self.teamsOfCurrPlayer[i] = teamSnapshot;
-          //console.log("teams snapshot changed", self.teamsOfCurrPlayer);
-        });
-        self.subscriptions.push(sub4);
-      }
-    });
-    self.subscriptions.push(sub3);
+    // this.afTeamsOfCurrPlayer = this.afGetTeamsOfPlayer(user.uid);
+    // let sub3 = this.afTeamsOfCurrPlayer.subscribe(teamIds => {
+    //   console.log("team of current player change ids", teamIds);
+    //   self.teamsOfCurrPlayer = [];
+    //   self.teamsOfCurrPlayer.length = teamIds.length;
+    //   for (let i = 0; i < teamIds.length; ++i) {
+    //     let tId = teamIds[i].$key;
+    //     let afTeam = self.afGetTeam(tId);
+    //     //console.log(tId, afTeam);
+    //     let sub4 = afTeam.subscribe(teamSnapshot => {
+    //       console.log("team snapshot changed", teamSnapshot);
+    //       self.teamsOfCurrPlayer[i] = teamSnapshot;
+    //       //console.log("teams snapshot changed", self.teamsOfCurrPlayer);
+    //     });
+    //     self.subscriptions.push(sub4);
+    //   }
+    // });
+    // self.subscriptions.push(sub3);
   }
 
   uninitialize() {
