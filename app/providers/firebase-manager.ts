9e4e4905c3d649d2656a080d6bed7a6d51668b12
ray-z <710956@gmail.com>
2016-08-23 21:58:58 +0800

    Added APIs to firebas-manager (i)

--- app/providers/firebase-manager.ts
+++ app/providers/firebase-manager.ts
@@ -8,30 +8,53 @@ export class FirebaseManager {
   constructor(private af: AngularFire) {
   }
 
-  // player
+  /********** All Get Operations ***********/
+
+  // player info
   getPlayerBasic(id: string) {
     return this.af.database.object(`/players/${id}/basic-info`);
   }
-
   getPlayerDetail(id: string) {
     return this.af.database.object(`/players/${id}/detail-info`);
   }
 
-  // team
+  // team info
   getTeamBasic(id: string) {
     return this.af.database.object(`/teams/${id}/basic-info`);
   }
-
   getTeamDetail(id: string) {
     return this.af.database.object(`/teams/${id}/detail-info`);
   }
 
-  // notifications
-  getNotificationList() {
+  // player: notifications
+  getMatchNotifications() {
     return this.af.database.list(`/players/{PLAYER_ID}/match-notifications`, {
       query: { orderByChild: 'time' }
     });
   }
 
+  // team: chatroom
+  getChatMessages() {
+    return this.af.database.list(`/teams/{TEAM_ID}/chatroom`);
+  }
+
+
+
+  /********** All Add Operations ***********/
+
+  // player: match notification
+  addMatchNotification(playerId: string, notificationKey:string, notification: any) {
+    this.af.database.object(`/players/${playerId}/match-notifications/${notificationKey}`).set(notification);
+  }
+
+  // team: new match
+  addNewMatch(match: any) {
+    this.af.database.list(`/teams/{TEAM_ID}/matches`).push(match);
+  }
+
+  // team: new chat message
+  addNewChatMessage(message: any) {
+    this.af.database.list(`/teams/{TEAM_ID}/chatroom`).push(message);
+  }
 
 }
