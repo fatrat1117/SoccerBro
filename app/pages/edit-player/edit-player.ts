e491ac30311e523d3a0bd3842a83f37f1afc8f47
fatrat1117 <fatrat1117@gmail.com>
2016-08-28 17:07:13 +0800

    edit player

--- app/pages/edit-player/edit-player.ts
+++ app/pages/edit-player/edit-player.ts
@@ -7,31 +7,31 @@ import {FirebaseManager} from '../../providers/firebase-manager';
   templateUrl: 'build/pages/edit-player/edit-player.html'
 })
 export class EditPlayerPage {
-  busy: boolean;
-  tId: any;
-  afTeam: any;
-  afTeamDetails: any;
-  team: any;
-  logoData: any;
-  logoUrl: any;
+ busy: boolean;
+  pId: any;
+   afPlayerBasic: any;
+  // afTeamDetails: any;
+   player: any;
+   logoData: any;
+   logoUrl: any;
 
   constructor(private am: AccountManager,
     private navParams: NavParams,
     private nav: NavController,
     private fm : FirebaseManager) {
-    this.tId = this.navParams.get('tId');
-    this.busy = false;
-    this.afTeam = this.fm.getTeamBasic(this.tId);
-    this.afTeamDetails = this.fm.getTeamDetail(this.tId);
-    this.team = {
-      tId: this.tId, 
-      logo: '',
+     this.pId = this.navParams.get('pId');
+     this.busy = false;
+     this.afPlayerBasic = this.fm.getPlayerBasic(this.pId);
+    // this.afTeamDetails = this.fm.getTeamDetail(this.tId);
+    this.player = {
+      pId: this.pId, 
+      photo: '',
       name: '',
       description: ''
     };
   }
 
-  changeLogo() {
+  changePhoto() {
     let self = this;
     self.busy = true;
 
@@ -47,35 +47,35 @@ export class EditPlayerPage {
     this.am.selectImgGetData(success, error);
   }
 
-  saveTeam(teamObj) {
-    let self = this;
-    let success = _ => {
-      self.nav.pop();
-      //self.team.logo = self.team.name = self.team.description = '';
-      //self.busy = false;
-    }
-    let error = err => {
-      alert(err);
-      self.busy = false;
-    }
-    this.am.updateTeam(teamObj, success, error);
-  }
+  // saveTeam(teamObj) {
+  //   let self = this;
+  //   let success = _ => {
+  //     self.nav.pop();
+  //     //self.team.logo = self.team.name = self.team.description = '';
+  //     //self.busy = false;
+  //   }
+  //   let error = err => {
+  //     alert(err);
+  //     self.busy = false;
+  //   }
+  //   this.am.updateTeam(teamObj, success, error);
+  // }
 
-  save() {
-    let self = this;
-    this.busy = true;
-    if (this.logoData) {
-        let success = imgUrl => {
-            self.team.logo = imgUrl;
-            self.saveTeam(self.team);
-        }
+  // save() {
+  //   let self = this;
+  //   this.busy = true;
+  //   if (this.logoData) {
+  //       let success = imgUrl => {
+  //           self.team.logo = imgUrl;
+  //           self.saveTeam(self.team);
+  //       }
 
-        let error = err => alert(err);
-        this.am.updateImgGetUrl(this.logoData, success, error);
-    } else {
-      self.saveTeam(self.team);
-    }
+  //       let error = err => alert(err);
+  //       this.am.updateImgGetUrl(this.logoData, success, error);
+  //   } else {
+  //     self.saveTeam(self.team);
+  //   }
 
-    //console.log(this.team);
-  }
+  //   //console.log(this.team);
+  // }
 }
