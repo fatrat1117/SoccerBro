cfe230da79ed3e393e5c5a3d165c829cce2f39a9
fatrat1117 <fatrat1117@gmail.com>
2016-09-03 11:34:08 +0800

    chart

--- app/pages/my-player/my-player.ts
+++ app/pages/my-player/my-player.ts
@@ -1,12 +1,16 @@
 import {Component} from '@angular/core';
+import {CORE_DIRECTIVES, FORM_DIRECTIVES, NgClass} from '@angular/common';
 import {NavController, NavParams} from 'ionic-angular';
+import {CHART_DIRECTIVES} from 'ng2-charts/ng2-charts';
 import {AngularFire, FirebaseListObservable, FirebaseObjectObservable} from 'angularfire2';
 import {AccountManager} from "../../providers/account-manager";
 import {FirebaseManager} from "../../providers/firebase-manager";
 import {ManagePlayerPage} from '../manage-player/manage-player';
 
+
 @Component({
-  templateUrl: 'build/pages/my-player/my-player.html'
+  templateUrl: 'build/pages/my-player/my-player.html',
+  directives: [CHART_DIRECTIVES, NgClass, CORE_DIRECTIVES, FORM_DIRECTIVES]
 })
 export class MyPlayerPage {
   pId: any;
@@ -14,6 +18,14 @@ export class MyPlayerPage {
   afBasic: any;
   afDetail: any;
   afPublic: any;
+  // Radar
+  public radarChartLabels:string[] = ['Eating', 'Drinking', 'Sleeping', 'Designing', 'Coding', 'Cycling', 'Running'];
+
+  public radarChartData:any = [
+    {data: [65, 59, 90, 81, 56, 55, 40], label: 'Series A'},
+    {data: [28, 48, 40, 19, 96, 27, 100], label: 'Series B'}
+  ];
+  public radarChartType:string = 'radar';
 
   constructor(private nav: NavController,
     private am: AccountManager,
@@ -26,6 +38,15 @@ export class MyPlayerPage {
     this.fm.increasePopularity(this.afPublic);
   }
 
+  // events
+  public chartClicked(e:any):void {
+    console.log(e);
+  }
+
+  public chartHovered(e:any):void {
+    console.log(e);
+  }
+  
   // editTeam() {
   //   this.nav.push(EditTeamPage,
   //     {
