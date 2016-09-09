a653c2db4950f4295108cb50a68190c58864ed4a
ray-z <710956@gmail.com>
2016-09-09 23:46:16 +0800

    My-team page, done.

--- app/pages/my-team/my-team.ts
+++ app/pages/my-team/my-team.ts
@@ -2,9 +2,12 @@ import {Component} from '@angular/core';
 import {NavController, ModalController, NavParams, Button} from 'ionic-angular';
 import {AngularFire, FirebaseListObservable, FirebaseObjectObservable} from 'angularfire2';
 import {AccountManager} from "../../providers/account-manager";
-import {EditTeamPage} from '../edit-team/edit-team';
 import {FirebaseManager} from "../../providers/firebase-manager";
 import {ManagePlayerPage} from '../manage-player/manage-player';
+import {EditPlayerPage} from '../edit-player/edit-player';
+import {ManageTeamPage} from '../manage-team/manage-team';
+import {CreateTeamPage} from '../create-team/create-team';
+import {EditTeamPage} from '../edit-team/edit-team';
 
 
 @Component({
@@ -16,9 +19,9 @@ export class MyTeamPage {
   tId: any;
   team: any;
   teamCaptain: any;
-  teamAvatar:any;
-  teamPlayers:any;
-  teamPlayersNumber:any;
+  teamAvatar: any;
+  teamPlayers: any;
+  teamPlayersNumber: any;
 
 
 
@@ -26,8 +29,8 @@ export class MyTeamPage {
   afTeam: FirebaseObjectObservable<any>;
   afTeamCaptain: FirebaseObjectObservable<any>;
 
-  afTeamDetail:FirebaseObjectObservable<any>;
-  afTeamPublic:FirebaseObjectObservable<any>;
+  afTeamDetail: FirebaseObjectObservable<any>;
+  afTeamPublic: FirebaseObjectObservable<any>;
   //afTeamPlayers:FirebaseListObservable<any>;
 
   // floating menu
@@ -45,14 +48,14 @@ export class MyTeamPage {
     var teamLogoURL = "";
     //getCaptainAFObject
     this.afTeam.subscribe(snapshot => {
-          this.team = snapshot;
-          var captainId= snapshot.captain;
-          teamLogoURL = snapshot.logo;
-          console.log(teamLogoURL);
-          this.afTeamCaptain = this.am.afGetPlayerById(captainId);
-          this.afTeamCaptain.subscribe(playerSnapshot => {
-            this.teamCaptain = playerSnapshot;
-          });
+      this.team = snapshot;
+      var captainId = snapshot.captain;
+      teamLogoURL = snapshot.logo;
+      console.log(teamLogoURL);
+      this.afTeamCaptain = this.am.afGetPlayerById(captainId);
+      this.afTeamCaptain.subscribe(playerSnapshot => {
+        this.teamCaptain = playerSnapshot;
+      });
     });
     // this.afTeamCaptain = this.am.afGetTeamCaptain(this.afTeam.)
     // console.log("123");
@@ -67,16 +70,29 @@ export class MyTeamPage {
     var number;
     let afTeamPlayers = this.fm.getPlayers(this.tId);
     let subPlayer = afTeamPlayers.subscribe(snapshot => {
-        console.log(this.tId);
-        this.teamPlayersNumber = snapshot.length;
-        this.teamPlayers = snapshot;
+      console.log(this.tId);
+      this.teamPlayersNumber = snapshot.length;
+      this.teamPlayers = snapshot;
     });
 
     //console.log(this.teamPlayersNumber);
     this.isOpen = false;
   }
 
+  manageTeam() {
+    this.nav.push(ManageTeamPage, {
+      id: this.am.currentUser.uid,
+    });
+  }
+
+  addNewTeam() {
+    let modal = this.modalController.create(CreateTeamPage);
+    modal.present();
+  }
+
   editTeam() {
+    console.log("edit");
+    
     this.nav.push(EditTeamPage,
       {
         tId: this.tId
@@ -84,17 +100,20 @@ export class MyTeamPage {
     );
   }
 
-  getTeamAvatar(src){
+
+
+
+  getTeamAvatar(src) {
     var image = new Image();
     image.src = src
     return image;
   }
 
   goManagePlayerPage() {
-    this.nav.push(ManagePlayerPage, {tId: this.tId});
+    this.nav.push(ManagePlayerPage, { tId: this.tId });
   }
 
-  invitePlayer(){
+  invitePlayer() {
 
   }
 
