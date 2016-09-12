5138612d207b66c7d7b10681957a1253082789d9
fatrat1117 <fatrat1117@gmail.com>
2016-09-12 15:09:27 +0800

    use temp virable

--- app/providers/account-manager.ts
+++ app/providers/account-manager.ts
@@ -15,7 +15,7 @@ declare let firebase: any;
 export class AccountManager {
   currentUser: any;
   afTeams: FirebaseListObservable<any>;
-  afCurrPlayer: FirebaseObjectObservable<any>;
+  //afCurrPlayer: FirebaseObjectObservable<any>;
   afCurrTeam: FirebaseObjectObservable<any>;
   afTeamsOfCurrPlayer: FirebaseListObservable<any>;
   currPlayer: any;
@@ -35,10 +35,9 @@ export class AccountManager {
     //if user is logged in first time, save default photo and name.
     let self = this;
     this.currentUser = this.getFbUser();
-    this.afCurrPlayer = this.afGetCurrentPlayer();
-    let sub = this.afCurrPlayer.subscribe(currPlayerData => {
+    let afCurrPlayer = this.afGetCurrentPlayer();
+    afCurrPlayer.subscribe(currPlayerData => {
       console.log("current player changed", currPlayerData);
-
       if (currPlayerData) {
         //player exists
         if (currPlayerData.displayName) {
@@ -50,7 +49,7 @@ export class AccountManager {
         else {
           console.log("first time login");
           //todo
-          self.afCurrPlayer.update({
+          afCurrPlayer.update({
             photoURL: user.photoURL || 'img/none.png',
             displayName: user.displayName || user.email
           }).catch(err => error(err));
@@ -59,7 +58,31 @@ export class AccountManager {
         }
       }
     });
-    self.subscriptions.push(sub);
+    // this.afCurrPlayer = this.afGetCurrentPlayer();
+    // let sub = this.afCurrPlayer.subscribe(currPlayerData => {
+    //   console.log("current player changed", currPlayerData);
+
+    //   if (currPlayerData) {
+    //     //player exists
+    //     if (currPlayerData.displayName) {
+    //       self.currPlayer = currPlayerData;
+    //       self.fm.selfTeamId = currPlayerData.teamId;
+    //       self.fm.selfId = user.uid;
+    //       success();
+    //     }
+    //     else {
+    //       console.log("first time login");
+    //       //todo
+    //       self.afCurrPlayer.update({
+    //         photoURL: user.photoURL || 'img/none.png',
+    //         displayName: user.displayName || user.email
+    //       }).catch(err => error(err));
+    //       //update player public
+    //       self.fm.getPlayerPublic(user.uid).update({ popularity: 1 });
+    //     }
+    //   }
+    // });
+    // self.subscriptions.push(sub);
 
     window["plugins"].OneSignal.getIds(ids => {
       self.fm.getPlayerDetail(user.uid).update({ pushId: ids.userId });
@@ -73,7 +96,7 @@ export class AccountManager {
       this.subscriptions[i].unsubscribe();
     }
     this.subscriptions = [];
-    this.afCurrPlayer = null;
+    //this.afCurrPlayer = null;
     this.afCurrTeam = null;
     this.currentUser = null;
     this.currTeam = null;
