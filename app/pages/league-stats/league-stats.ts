9d2b90af111afc4157fdffd83a2bfdc084c2bf28
fatrat1117 <fatrat1117@gmail.com>
2016-10-13 11:50:41 +0800

    show today

--- app/pages/league-stats/league-stats.ts
+++ app/pages/league-stats/league-stats.ts
@@ -4,6 +4,7 @@ import {FirebaseManager} from '../../providers/firebase-manager';
 import {StringToDatePipe, NumberToTimePipe} from '../../pipes/moment.pipe';
 import {Subject} from 'rxjs/Subject';
 import {TeamBasicPipe} from '../../pipes/team-basic.pipe';
+import * as moment from 'moment';
 
 @Component({
   templateUrl: 'build/pages/league-stats/league-stats.html',
@@ -16,6 +17,7 @@ export class LeagueStatsPage {
   dates: any;
   afMatches: any;
   dateSubject = new Subject();
+  today = moment(moment().format("YYYY-MM-DD")).unix() * 1000;
 
   constructor(private nav: NavController,
   private fm: FirebaseManager) {
@@ -65,6 +67,7 @@ export class LeagueStatsPage {
       this.dates = dates;
       console.log(dates)});
     this.afMatches = fm.queryMatches(this.dateSubject);
+    console.log('today', this.today);
   }
 
   testClick(){
